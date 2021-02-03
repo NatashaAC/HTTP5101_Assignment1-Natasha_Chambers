ddc5e6647461bf7fc9b0c5049be30396f341e289
Natasha Chambers <natashaagathachambers@gmail.com>
2021-02-03 11:50:31 -0500

    Updated controllers for question 1 and question 2

--- HTTP5101_Assignment1-Natasha_Chambers/Controllers/AddTenController.cs
+++ HTTP5101_Assignment1-Natasha_Chambers/Controllers/AddTenController.cs
@@ -9,17 +9,27 @@ namespace HTTP5101_Assignment1_Natasha_Chambers.Controllers
 {
     public class AddTenController : ApiController
     {
-        //<summary>
-        //Returns the integer input (id) plus 10
-        //</summary>
-        //
-        //Examples/Tests
-        //GET api/addten/21
-        //GET api/addten/0
-        //GET api/addten/-9
+        /*
+         * Question 1 Objective:
+         * Create a method that returns an amount that is 
+         * ten more than the orignal amount entered
+         */
+
+        /// <summary>
+        /// This method will take in an input for a number  
+        /// add ten to it and then output the total
+        /// </summary>
+        /// <param name="id">input of a random number</param>
+        /// <returns>a new number</returns>
+        /// <example>
+        /// GET api/AddTen/21 -> 31
+        /// GET api/AddTen/0 -> 20
+        /// GET api/AddTen/-9 -> 10
+        /// </example>
         public int Get(int id)
         {
-            return id + 10;
+            int total = id + 10;
+            return total;
         }
     }
 }
--- HTTP5101_Assignment1-Natasha_Chambers/Controllers/SquareController.cs
+++ HTTP5101_Assignment1-Natasha_Chambers/Controllers/SquareController.cs
@@ -9,14 +9,23 @@ namespace HTTP5101_Assignment1_Natasha_Chambers.Controllers
 {
     public class SquareController : ApiController
     {
-        //<summary>
-        //Returns the square of the integer input (id)
-        //</summary>
-        //
-        //Examples/Tests
-        //GET api/square/2
-        //GET api/square/-2
-        //GET api/square/10
+        /*
+         * Question 2 Objective:
+         * Create a method that returns an amount that is 
+         * the orignal amount squared
+         */
+
+        /// <summary>
+        /// This method will take in an input for a number  
+        /// square it and then output the squared number
+        /// </summary>
+        /// <param name="id">input of a number</param>
+        /// <returns>a new number</returns>
+        /// <example>
+        /// GET api/AddTen/2 -> 4
+        /// GET api/AddTen/-2 -> 4
+        /// GET api/AddTen/10 -> 100
+        /// </example>
         public int Get(int id)
         {
             return id * id;
